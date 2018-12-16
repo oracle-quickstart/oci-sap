7c5118eb532b434b0961485f184b48ea8cc654f1
flavio.pereira <flavio.pereira@oracle.com>
2018-12-16 16:35:05 -0600

    Added runlevel 5 and FSS for DB

--- single-ad/remote-exec.tf
+++ single-ad/remote-exec.tf
@@ -48,6 +48,7 @@ resource "null_resource" "connect_to_sap_app_instance" {
       "echo ${local.sap_fss_mount_target_ip_address}:${var.export_path_fss_sap} ${var.export_path_fss_sap} nfs tcp,vers=3 | sudo tee -a /etc/fstab",
       "sudo growpart /dev/sda 3",
       "sudo yum groupinstall 'Server with GUI' -y",
+      "sudo systemctl set-default graphical.target",
     ]
   }
 }
@@ -76,8 +77,13 @@ resource "null_resource" "connect_to_sap_db_instance" {
     inline = [
       "chmod +x /tmp/script-sap-db.sh",
       "/tmp/script-sap-db.sh",
+      "sudo yum -y install nfs-utils > nfs-utils-install.log",
+      "sudo mkdir ${var.export_path_fss_sap}",
+      "sudo mount ${local.sap_fss_mount_target_ip_address}:${var.export_path_fss_sap} ${var.export_path_fss_sap}",
+      "echo ${local.sap_fss_mount_target_ip_address}:${var.export_path_fss_sap} ${var.export_path_fss_sap} nfs tcp,vers=3 | sudo tee -a /etc/fstab",
       "sudo growpart /dev/sda 3",
       "sudo yum groupinstall 'Server with GUI' -y",
+      "sudo systemctl set-default graphical.target",
     ]
   }
 }
--- single-ad/script-sap-app.sh
+++ single-ad/script-sap-app.sh
@@ -32,32 +32,32 @@ if [ $size -lt $value ]
         sudo echo "UUID=`sudo blkid /dev/vg_sap/oracle_client | cut -d ':'  -f2 | cut -d '=' -f2 | cut -d '"' -f2`  /oracle/client xfs defaults,_netdev 0 2" >> /etc/fstab
         sudo chmod 600 /etc/fstab
 else
-		echo -e "o\nn\np\n1\n\n\nw" | sudo fdisk /dev/sdb
-		sudo mkswap /dev/sdb1
-		sudo chmod 777 /etc/fstab
-		sudo echo "UUID=`sudo blkid /dev/sdb1 | cut -d ':'  -f2 | cut -d '=' -f2 | cut -d '"' -f2`  swap swap defaults,_netdev,x-initrd.mount 0 0" >> /etc/fstab
-		sudo chmod 600 /etc/fstab
-		sudo swapoff -a
-		sudo swapon -a
-        echo -e "o\nn\np\n1\n\n\nw" | sudo fdisk /dev/sdc
-        sudo pvcreate /dev/sdc1
-        sudo vgcreate vg_sap /dev/sdc1
-        sudo lvcreate -L 30GB -n usr_sap vg_sap
-        sudo lvcreate -L 20GB -n usr_sap_trans vg_sap
-        sudo lvcreate -L 5GB -n oracle_client vg_sap
-        sudo mkfs.xfs /dev/vg_sap/usr_sap
-        sudo mkfs.xfs /dev/vg_sap/usr_sap_trans
-        sudo mkfs.xfs /dev/vg_sap/oracle_client
-        sudo mkdir -p /usr/sap
-        sudo mount /dev/vg_sap/usr_sap /usr/sap
-        sudo mkdir -p /usr/sap/trans
-        sudo mount /dev/vg_sap/usr_sap_trans /usr/sap/trans
-        sudo mkdir -p /oracle/client
-        sudo mount /dev/vg_sap/oracle_client /oracle/client
-        sudo chmod 777 /etc/fstab
-        sudo echo "UUID=`sudo blkid /dev/vg_sap/usr_sap | cut -d ':'  -f2 | cut -d '=' -f2 | cut -d '"' -f2`  /usr/sap xfs defaults,_netdev 0 2" >> /etc/fstab
-        sudo echo "UUID=`sudo blkid /dev/vg_sap/usr_sap_trans | cut -d ':'  -f2 | cut -d '=' -f2 | cut -d '"' -f2`  /usr/sap/trans xfs defaults,_netdev 0 2" >> /etc/fstab
-        sudo echo "UUID=`sudo blkid /dev/vg_sap/oracle_client | cut -d ':'  -f2 | cut -d '=' -f2 | cut -d '"' -f2`  /oracle/client xfs defaults,_netdev 0 2" >> /etc/fstab
-        sudo chmod 600 /etc/fstab
+	echo -e "o\nn\np\n1\n\n\nw" | sudo fdisk /dev/sdb
+	sudo mkswap /dev/sdb1
+	sudo chmod 777 /etc/fstab
+	sudo echo "UUID=`sudo blkid /dev/sdb1 | cut -d ':'  -f2 | cut -d '=' -f2 | cut -d '"' -f2`  swap swap defaults,_netdev,x-initrd.mount 0 0" >> /etc/fstab
+	sudo chmod 600 /etc/fstab
+	sudo swapoff -a
+	sudo swapon -a
+       echo -e "o\nn\np\n1\n\n\nw" | sudo fdisk /dev/sdc
+       sudo pvcreate /dev/sdc1
+       sudo vgcreate vg_sap /dev/sdc1
+       sudo lvcreate -L 30GB -n usr_sap vg_sap
+       sudo lvcreate -L 20GB -n usr_sap_trans vg_sap
+       sudo lvcreate -L 5GB -n oracle_client vg_sap
+       sudo mkfs.xfs /dev/vg_sap/usr_sap
+       sudo mkfs.xfs /dev/vg_sap/usr_sap_trans
+       sudo mkfs.xfs /dev/vg_sap/oracle_client
+       sudo mkdir -p /usr/sap
+       sudo mount /dev/vg_sap/usr_sap /usr/sap
+       sudo mkdir -p /usr/sap/trans
+       sudo mount /dev/vg_sap/usr_sap_trans /usr/sap/trans
+       sudo mkdir -p /oracle/client
+       sudo mount /dev/vg_sap/oracle_client /oracle/client
+       sudo chmod 777 /etc/fstab
+       sudo echo "UUID=`sudo blkid /dev/vg_sap/usr_sap | cut -d ':'  -f2 | cut -d '=' -f2 | cut -d '"' -f2`  /usr/sap xfs defaults,_netdev 0 2" >> /etc/fstab
+       sudo echo "UUID=`sudo blkid /dev/vg_sap/usr_sap_trans | cut -d ':'  -f2 | cut -d '=' -f2 | cut -d '"' -f2`  /usr/sap/trans xfs defaults,_netdev 0 2" >> /etc/fstab
+       sudo echo "UUID=`sudo blkid /dev/vg_sap/oracle_client | cut -d ':'  -f2 | cut -d '=' -f2 | cut -d '"' -f2`  /oracle/client xfs defaults,_netdev 0 2" >> /etc/fstab
+       sudo chmod 600 /etc/fstab
  fi 
        
