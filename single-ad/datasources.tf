d1464519a56bec63b3c4684a8157d859c537cca4
Sergio Leunissen <github@accounts.metic.org>
2019-04-23 13:29:51 -0400

    Fix license header

--- single-ad/datasources.tf
+++ single-ad/datasources.tf
@@ -1,5 +1,5 @@
 ## Copyright © 2019, Oracle and/or its affiliates. 
-## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
+## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at https://oss.oracle.com/licenses/upl
 
 # Get list of Availability Domains
 data "oci_identity_availability_domains" "ADs" {
