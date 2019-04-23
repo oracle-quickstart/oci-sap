34992e887ce2506a7d9394c1af11f0b9052cde5b
Sergio Leunissen <github@accounts.metic.org>
2019-04-23 13:35:52 -0400

    Fix license header

--- single-ad/variables.tf
+++ single-ad/variables.tf
@@ -1,5 +1,5 @@
 ## Copyright © 2019, Oracle and/or its affiliates. 
-## All rights reserved. The Universal Permissive License (UPL), Version 1.0
+## All rights reserved. The Universal Permissive License (UPL), Version 1.0 https://oss.oracle.com/licenses/upl/#_How_should_I
 
 variable "tenancy_ocid" {}
 variable "user_ocid" {}
